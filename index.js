376980dbac0a3362ac24436b056ec30ac41556bf
DEVESH TIWARI <deveshups@gmail.com>
2026-02-27 02:47:08 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  fs.readdir("./files", function (err, files) {
+    console.log(files);
+    res.render("index", { files: files });
+  });
+});
+
+app.post("/create", (req, res) => {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.details,
+    function (err) {
+      res.redirect("/");
+    },
+  );
+});
+
+app.listen(3000);
